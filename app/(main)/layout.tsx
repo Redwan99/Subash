eecf3521cb82b8d34e31edecefb3505bb723d999
Redwan Rashid Rico <22303307@iubat.edu>
2026-03-04 22:07:10 +0600

    Removed container padding

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -10,16 +10,16 @@ export default function MainLayout({
     return (
         <>
             {/* pb-32 protects content from being hidden behind the floating bottom nav on mobile */}
-            <div className="max-w-[1600px] mx-auto w-full flex justify-center px-4 sm:px-6 lg:px-8 pb-32 md:pb-6">
+            <div className="max-w-[1600px] mx-auto w-full flex justify-center pb-32 md:pb-6">
                 {/* Hide on mobile/tablet, show on lg desktop */}
-                <div className="hidden lg:block w-64 xl:w-72 shrink-0 py-6 pr-6">
+                <div className="hidden lg:block w-[var(--sidebar-width)] shrink-0">
                     <LeftSidebar />
                 </div>
 
-                <div className="flex-1 w-full py-6 min-h-screen animate-fade-in-up">{children}</div>
+                <div className="flex-1 w-full py-6 px-4 sm:px-6 min-h-screen animate-fade-in-up">{children}</div>
 
                 {/* Hide on mobile/tablet/small desktop, show ONLY on xl Mac/PC */}
-                <div className="hidden xl:block w-80 shrink-0 py-6 pl-6">
+                <div className="hidden xl:block w-[var(--sidebar-right-width)] shrink-0">
                     <RightSidebar />
                 </div>
             </div>
